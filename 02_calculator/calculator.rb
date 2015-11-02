19ffb56d1e6edf3b97885d2131b0c0ecbe8bb17e
Mike Cooper <shivagogo@gmail.com>
2015-11-02 17:25:19 -0500

    Error free

---
+++ 02_calculator/calculator.rb
@@ -0,0 +1,15 @@
+def add(first, second)
+	first + second
+end
+
+def subtract(first, second)
+	first - second
+end
+
+def sum(numbers)
+	total = 0
+	numbers.each do |numerals|
+	total += numerals
+end
+	total
+end
